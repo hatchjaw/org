471b1fa1fd67bf0ac4a565627f30a6bf756092bf
Tommy Rushton <hatchjaw@users.noreply.github.com>
2026-03-31 22:53:38 +0200

    Thesis outline taking shape.

--- thesis/.dir-locals.el
+++ thesis/.dir-locals.el
@@ -11,8 +11,7 @@
                         (when (org-export-derived-backend-p backend 'latex)
                           (replace-regexp-in-string
                            "\\\\chapter\\*{\\(Abstract\\)}"
-                           "\\\\newpage
-\\\\section*{\\1}"
+                           "\\\\newpage\n\\\\section*{\\1}"
                            headline))))
 
               (eval . (defun tar/intro-in-toc (headline backend info)
@@ -20,6 +19,14 @@
                         (when (org-export-derived-backend-p backend 'latex)
                           (replace-regexp-in-string
                            "\\(\\\\label{sec:intro}\\)"
-                           "\\1
-\\\\addcontentsline{toc}{chapter}{\\\\nameref{sec:intro}}"
-                           headline)))))))
+                           "\\1\n\\\\addcontentsline{toc}{chapter}{\\\\nameref{sec:intro}}"
+                           headline))))
+
+              ;; (eval . (defun tar/display-minitoc (headline backend info)
+              ;;           "Add minitoc to the start of each chapter."
+              ;;           (when (org-export-derived-backend-p backend 'latex)
+              ;;             (replace-regexp-in-string
+              ;;              "\\(\\\\chapter{.+}\n\\\\label{.+}\\)"
+              ;;              "\\1\n\\\\minitoc\n\\\\newpage"
+              ;;              headline))))
+              )))
